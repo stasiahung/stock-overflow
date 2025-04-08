c00658eb093d38f8f806d33a6db9724ae6308e01
Stasia Hung <stasia_hung@tomofun.com>
2025-04-08 21:30:26 +0800

    Add toolbar

--- stockoverflow/EditStrategyView.swift
+++ stockoverflow/EditStrategyView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct EditStrategyView: View {
+    @State var strategyName: String = ""
+    
+    @Environment(\.dismiss) private var dismiss
+    //    @Environment(\.modelContext) var context
+    
     var body: some View {
-        VStack {
-            CategoryCardView(categoryName: "基本面", color: .red, onClick: {})
-            CategoryCardView(categoryName: "技術面", color: .red, onClick: {})
-            CategoryCardView(categoryName: "籌碼面", color: .red, onClick: {})
+        NavigationStack {
+            VStack {
+                TextField("策略名稱", text: $strategyName)
+                    .padding()
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(8)
+                CategoryCardView(categoryName: "基本面", color: .orange, onClick: {})
+                CategoryCardView(categoryName: "技術面", color: .pink, onClick: {})
+                CategoryCardView(categoryName: "籌碼面", color: .red, onClick: {})
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("新增策略")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Button("Save") {
+                        dismiss()
+                    }
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
